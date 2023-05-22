0de0a0b76876a99101ee9ef513602d478187009a
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-05-22 10:25:51 -0400

    minor code changes made

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const path = require('path');
 const api=require("./routes/routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
-app.use(check);
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api",api);
