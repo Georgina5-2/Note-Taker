83c7e5f9d4e43a96098f1ccacc7f07181f3731b9
Georgina5-2 <122113060+Georgina5-2@users.noreply.github.com>
2023-06-11 01:08:36 -0400

    Code changes added

--- routes/notes.js
+++ routes/notes.js
@@ -1,64 +1,67 @@
-const notes=require('express').Router();
-const{v4:uuidv4}=require("uuid");
-const fs=require("fs");
+const router = require('express').Router();
+const { v4: uuidv4 } = require("uuid");
+const fs = require("fs");
 
-notes.get("/",(req,res)=>{
-    fs.readFile('./db/notes.json',(error,data)=>{
-        if(error){
+router.get("/", (req, res) => {
+    fs.readFile('db/notes.json','utf-8',(error, data) => {
+        if (error) {
             console.log(error);
         }
-        else{
-             res.json(JSON.parse(data));
+        else {
+            res.json(JSON.parse(data));
         }
-    })
     });
+});
 
-    notes.post("/",(req,res)=>{
-        console.log("post received req.body =", req.body)
-        const{title,text}=req.body;
-        const newNote={
-            note_id:uuidv4(),
-            title,
-            text,
-        };
-        fs.readFile("./db/notes.json",(error,data)=>{
-            if(error){
-                console.log(error);
-            }
-            else{
-                console.log("in else");
-                const parsedData=JSON.parse(data);
-                parsedData.push(newNote);
-                fs.writeFile('./db/notes.json',JSON.stringify(parsedData),(writeError)=>{
-                    if(writeError){
-                        console.log(error);
-                    }
-                    else{
-                        console.log("The notes are saved");
-                    }
-                })
-            }
-        })
-
-        const response={status:"success",
-                        body:newNote
+router.post("/", (req, res) => {
+    console.log("post received req.body =", req.body);
+    const { title, text } = req.body;
+    const newNote = {
+        id: uuidv4(),
+        title,
+        text,
+    };
+    fs.readFile("./db/notes.json", (error, data) => {
+        if (error) {
+            console.log(error);
         }
-        res.status(200).json(response);
-
-
-    });
-
-    notes.delete("/:noteId",(req,res)=>{
-        const noteToBeDeleted=req.params.noteId;
-        fs.readFile("./db/notes.json",(error,data)=>{
-            const parsedData=JSON.parse(data);
-            const result=parsedData.filter((note)=>note.note_id!==noteToBeDeleted);
-            fs.writeFile("./db/notes.json",JSON.stringify(result),(error)=>{
-                if(error){
+        else {
+            console.log("in else");
+            const parsedData = JSON.parse(data);
+            parsedData.push(newNote);
+            fs.writeFile('./db/notes.json', JSON.stringify(parsedData), (writeError) => {
+                if (writeError) {
                     console.log(error);
                 }
-            })
-        })
+                else {
+                    console.log("The notes are saved");
+                }
+            });
+        }
     });
 
-    module.exports=notes;
+    const response = {
+        status: "success",
+        body: newNote
+    };
+    res.status(200).json(response);
+
+
+});
+
+router.delete("/:id", (req, res) => {
+    const noteToBeDeleted = req.params.id;
+    fs.readFile("db/notes.json", (error, data) => {
+        const parsedData = JSON.parse(data);
+        const result = parsedData.filter((note) => note.id !== noteToBeDeleted);
+        console.log("result",result);
+        fs.writeFile("db/notes.json", JSON.stringify(result), (error) => {
+            if (error) {
+                console.log(error);
+            }
+           // res.end()
+        });
+    });
+});
+
+module.exports = router;
